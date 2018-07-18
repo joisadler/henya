693640312d8cd23f9c412ff1c6f3d289063f3509
Yossi Adler <joisadler@gmail.com>
2018-07-18 15:32:29 +0300

    fix menu icon layout on tablets and mobile devices

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -290,10 +290,29 @@ Object.defineProperty(exports, "__esModule", {
 
 exports.default = function () {
   var menuButton = document.querySelector('.btn-menu');
+  var ticker = document.querySelector('.hidden-menu-ticker');
+
   if (/Android|webOS|iPhone|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     // device is a mobile phone
-    menuButton.style.top = 'calc(15.5vh / 3)';
+    if (screen.width >= 768) {
+      menuButton.style.top = 'calc(15.5vh / 3)';
+    }
   }
+
+  ticker.addEventListener('change', function () {
+    if (ticker.checked) {
+      menuButton.style.top = 0;
+    } else if (/Android|webOS|iPhone|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      // device is a mobile phone
+      if (screen.width >= 768) {
+        menuButton.style.top = 'calc(15.5vh / 3)';
+      }
+      menuButton.style.top = '6vmin';
+    } else {
+      // device is a tablet
+      menuButton.style.top = 'calc((15.5vh / 3) + 4.5vh)';
+    }
+  });
 };
 
 /***/ }),
