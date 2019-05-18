a6004f9d39e517ecb37a6ec1505f58547d7bdf75
Yossi Adler <joisadler@gmail.com>
2019-05-18 20:02:18 +0300

    alert the user when the form has been submitted

--- src/javascripts/contact.js
+++ src/javascripts/contact.js
@@ -17,6 +17,7 @@ export default () => {
         data: $form.serialize()
       });// .done(response => console.log(response));
       textarea.value = text;
+      alert('Your message has been sent successfully!');
     }
   };
 
